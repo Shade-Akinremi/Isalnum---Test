632b25f5845ce442e9a145cb00f6771f4510fe12
Shade-Akinremi <147956306+Shade-Akinremi@users.noreply.github.com>
2024-03-20 15:31:12 +0100

    Add files via upload

---
+++ Isalnum () Test.c
@@ -0,0 +1,16 @@
+/* 
+Shade Akinremi
+18/03/2024
+This code is to test how the function isalnum() works
+and check if a character is alphanumeric
+*/
+#include<stdio.h>
+#include<ctype.h>
+
+int main()
+{
+    printf("Please enter a character");
+    int character = putchar();
+    
+    return 0;
+}
